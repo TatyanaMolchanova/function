22f0fcec3064924e2bdaad36ed33cecf34290590
Molchanova Tatyana <mtuspeh@gmail.com>
2019-07-25 22:10:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+(function(){
+	'use strict';
+
+	if (false) {
+
+		function ucFirst(str) {
+
+			var firstLetter = str.charAt(0).toUpperCase();
+
+			console.log(firstLetter);
+
+			var remaindOfWord = str.slice(1);
+
+			console.log(remaindOfWord);
+
+			var wholeWord = firstLetter + remaindOfWord;
+
+			alert(wholeWord);
+
+			// str.slice(1);
+			
+		}
+
+		ucFirst('');
+	}
+
+	if (false) {
+
+		function min(a,b) {
+			if (a < b) {
+				return a;
+			} else {
+				return b;
+			}
+
+		}
+
+		alert( min(1, 1) );
+	}
+
+
+	if (false) {
+		nextPrime:
+		  for (var i = 2; i <= 10; i++) {
+
+		    for (var j = 2; j < i; j++) {
+		      if (i % j == 0) continue nextPrime;
+		    }
+
+		    console.log( i ); 
+		  }
+	}
+	
+
+	if (false) {
+
+
+		function bigNum(num) {
+
+			var enterNumber;
+
+			do {
+			  enterNumber = prompt("Введите число больше 100?", 0);
+			} while (enterNumber <= num && enterNumber != null);
+
+			
+		}
+	    
+	    bigNum(10);
+	}
+	
+
+})();
